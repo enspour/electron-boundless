d94a0b8f79e91b204ebca1fe20157edabafcfcd4
enspour <enspour@gmail.com>
2023-01-20 16:03:37 +0300

    updated: changed deck card in catalog

--- src/renderer/components/ui/catalog/DeckCard/DeckCard.tsx
+++ src/renderer/components/ui/catalog/DeckCard/DeckCard.tsx
@@ -1,12 +1,15 @@
-import { memo, FC, MouseEvent } from "react";
+import { useRef, memo, FC, MouseEvent } from "react";
 
 import { decksActions, useAppDispatch } from "@redux";
 
 import Icon from "../Icon/Icon";
 
-import { Deck } from "@global/types";
+import useIsHover from "@hooks/css/useIsHover";
+
+import { Deck, Levels } from "@global/types";
 
 import RemoveIcon from "@assets/images/deck-card/remove.svg";
+import MoreIcon from "@assets/images/deck-card/more.svg";
 
 import {
     Colors,
@@ -19,17 +22,25 @@ import services from "@services";
 
 import styles from "./DeckCard.module.scss";
 
+const shortLevels: Record<Levels, string> = {
+    Beginners: "A1",
+    "Pre-intermediate": "A2",
+    Intermediate: "B1",
+    "Upper-intermediate": "B2",
+    Advanced: "C1",
+    Mastery: "C2",
+};
+
 interface DeckCardProps {
     deck: Deck;
     primary?: ThemeColor;
-    secondary?: ThemeColor;
 }
 
-const DeckCard: FC<DeckCardProps> = ({
-    deck,
-    primary = "primary",
-    secondary = "primary",
-}) => {
+const DeckCard: FC<DeckCardProps> = ({ deck, primary = "primary" }) => {
+    const cardRef = useRef();
+
+    const isHover = useIsHover(cardRef);
+
     const dispatch = useAppDispatch();
 
     const open = () => {
@@ -44,36 +55,43 @@ const DeckCard: FC<DeckCardProps> = ({
 
     return (
         <div
+            ref={cardRef}
             className={styles.card}
             style={{
                 color: Colors[primary],
                 backgroundColor: Backgrounds[primary],
-                border: `0.1rem solid ${BorderColors[primary]}`,
+                border: isHover
+                    ? `0.1rem solid ${BorderColors[primary]}`
+                    : `0.1rem solid ${Backgrounds[primary]}`,
             }}
             onClick={open}
         >
-            <div className={styles.card__wrapper}>
-                <div
-                    className={styles.card__level}
-                    style={{
-                        color: Colors[secondary],
-                        backgroundColor: Backgrounds[secondary],
-                    }}
-                >
-                    {deck.level}
-                </div>
-
+            <div
+                className={styles.card__controls}
+                style={{
+                    visibility: isHover ? "visible" : "hidden",
+                }}
+            >
                 <Icon
                     icon={RemoveIcon}
                     width="1.2rem"
                     height="1.2rem"
                     onClick={remove}
                 />
+
+                <Icon icon={MoreIcon} width="1.2rem" height=".3rem" />
             </div>
 
-            <div className={styles.card__info}>
-                <div className={styles.card__name}>{deck.name}</div>
+            <div
+                className={styles.card__level}
+                style={{
+                    color: `${Colors[primary]}`,
+                }}
+            >
+                {shortLevels[deck.level]}
             </div>
+
+            <div className={styles.card__name}>{deck.name}</div>
         </div>
     );
 };
